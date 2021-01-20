4896e608f21a2a63b13a237544b593ca4d870d5c
Erlend <erlend.halvorsen.wow@gmail.com>
2021-01-20 01:29:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function renderProjects(projects) {
 
 
         const project = createProjectElement();
-
+        project.style ="display:flex;"
         project.querySelector("h2").innerText = element.name;
         project.querySelector("p").innerText = element.description;
         project.querySelector("tags").innerText = "Tags: " + element.tags;
