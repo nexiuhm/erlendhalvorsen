a514369bb04a1a118a4e0e52fadf9ecdb703dd4b
Erlend <erlend.halvorsen.wow@gmail.com>
2021-01-20 01:05:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,37 @@ fetch("data.json")
 .then(response => response.json())
 .then(data => {
 
-
-    data.projects.forEach(element => {
-
-        console.log(element)
-        
-    });
+    renderProjects(data.projects)
+   
 
 
 });
+
+
+renderProjects(projects) {
+
+
+    const projects_container= document.getElementByClassName("projects");
+    projects_container.innerHTML = "";
+    
+
+    
+
+    projects.forEach(element => {
+
+
+        const container = document.createElement("div");
+        const header = document.createElement("h2");
+        const img = document.createElement("img");
+        
+
+        header.innerText = element.name;
+        
+        container.addChild(header);
+        container.addChild(img);
+        projects_container.addChild(container);
+        
+    });
+
+}
 
