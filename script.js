a47e2f44195cac04739c3b6f6fb72ff9d7329101
Erlend <erlend.halvorsen.wow@gmail.com>
2021-01-20 01:27:49 +0100

    rr

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function renderProjects(projects) {
 
         project.querySelector("h2").innerText = element.name;
         project.querySelector("p").innerText = element.description;
-        
+        project.querySelector("tags").innerText = "Tags: " + element.tags;
         projects_container.appendChild(project);
         
     });
@@ -38,13 +38,16 @@ function renderProjects(projects) {
 function createProjectElement() {
 
     const container = document.createElement("div");
+
     const header = document.createElement("h2");
     const description = document.createElement("p");
     const img = document.createElement("img");
+    const tags = document.createElement("div");
 
     container.appendChild(header);
     container.appendChild(img);
     container.appendChild(description);
+    container.appendChild(tags);
 
     return container;
 }
