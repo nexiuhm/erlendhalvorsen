7df26de2d6c84bd81ce0ff0e526d58c38308f497
Erlend <erlend.halvorsen.wow@gmail.com>
2021-01-21 09:33:56 +0100

    ff

--- script.js
+++ script.js
@@ -1,56 +1,87 @@
-/* All data stored in a local json file, so its easier to implement a remote backend later */
 
-fetch("data.json")
-.then(response => response.json())
-.then(data => {
+{   // anonomous namespace ES5 style
 
-    renderProjects(data.projects)
+
    
+    const projects = null;
 
 
-});
+
+    /* Entrypoint*/
+    document.addEventListener("DOMContentLoaded", () => {
 
 
-function renderProjects(projects) {
-
-
-    const projects_container= document.querySelector(".projects");
-    /* Empty container everytime for a clean re-render */
-    projects_container.innerHTML = "";
+         /* All data stored in a local json file, so its easier to implement a remote backend later */
+        fetch("data.json")
+        .then(response => response.json())
+        .then(data => {
     
-
     
-
-    projects.forEach(element => {
-
-
-        const project = createProjectElement();
-        project.querySelector("h2").innerText = element.name;
-        project.querySelector("p").innerText = element.description;
-        project.querySelector("div").innerText = "Tags: " + element.tags;
-        projects_container.appendChild(project);
+            projects = data.projects;
+            renderProjects(projects)
         
-    });
+    
+    
+        });
+        
+    })
 
-}
+  
 
-/* Filter click handlers - todo */
-function onClickFilterByTag () {}
 
-/* Creates the HTML structure for a single prosject "card" */
-function createProjectElement() {
+    function renderProjects(projects) {
 
-    const container = document.createElement("div");
-    container.className = "project";
-    const header = document.createElement("h2");
-    const description = document.createElement("p");
-    const img = document.createElement("img");
-    const tags = document.createElement("div");
 
-    container.appendChild(header);
-    container.appendChild(img);
-    container.appendChild(description);
-    container.appendChild(tags);
+        const projects_container = document.querySelector(".projects");
+        /* Empty container everytime for a clean re-render */
+        projects_container.innerHTML = "";
+        
 
-    return container;
+        
+
+        projects.forEach(element => {
+
+
+            const project = createProjectElement();
+            project.querySelector("h2").innerText = element.name;
+            project.querySelector("p").innerText = element.description;
+            project.querySelector("div").innerText = "Tags: " + element.tags;
+            projects_container.appendChild(project);
+            
+        });
+
+    }
+
+    /* Filter click handlers - todo */
+    function onClickFilterByTag (e) {
+        console.log("onClickFilterByTag called by element: " + e.target)
+
+        const tag = "Programming" //test, fix later
+
+        const filteredProjectArray = projects.filter(project => project.tags.include(tag))
+
+        renderProjects(filteredProjectArray);
+    }
+
+    /* Creates the HTML structure for a single prosject "card" */
+    function createProjectElement() {
+
+        const container = document.createElement("div");
+        container.className = "project";
+        const header = document.createElement("h2");
+        header.className = "project-title";
+        const description = document.createElement("p");
+        description.className
+        const img = document.createElement("img");
+        const tags = document.createElement("span");
+        const github = document.createElement("span");
+
+        container.appendChild(header);
+        container.appendChild(img);
+        container.appendChild(description);
+        container.appendChild(tags);
+
+        return container;
+    }
+
 }
